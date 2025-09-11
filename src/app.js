a3a8b02e7a5532f774b7c68e78ea982e1912dd09
Abdelaziz Mohamed <abdelazizmo2022@gmail.com>
2025-09-12 01:47:50 +0300

    Commit

--- src/app.js
+++ src/app.js
@@ -19,7 +19,14 @@ const app = express();
 
 // Security & basics
 app.use(helmet());
-app.use(cors({ origin: CORS_ORIGIN, credentials: true }));
+app.use(
+  cors({
+    origin: CORS_ORIGIN,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
