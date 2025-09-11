cf651c4f6af4c862d487125048a53ede2280b9c1
Abdelaziz Mohamed <abdelazizmo2022@gmail.com>
2025-09-12 02:06:53 +0300

    Commit

--- api/index.js
+++ api/index.js
@@ -8,5 +8,17 @@ module.exports = async (req, res) => {
     await connectDB();
     isConnected = true;
   }
+
+  if (req.method === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", process.env.CORS_ORIGIN || "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Authorization"
+    );
+    return res.status(200).end();
+  }
+
   return app(req, res);
 };
